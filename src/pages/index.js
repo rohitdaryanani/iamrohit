8b5ea538db5e64dc09fa7010bbd262c0646d9d2b
Rohit Daryanani <rohitdaryanani@yahoo.com>
2020-04-17 21:12:38 +0800

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,25 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div
+      style={{
+        margin: "0px auto",
+        height: "100vh",
+        maxWidth: "960px",
+        padding: "0px 1.0875rem 1.45rem",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1>I'm Rohit /Ro-hit/</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
